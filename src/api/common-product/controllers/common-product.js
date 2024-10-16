3d5ec193ffffd1da24902305730fc8a74d470331
Mehul Nahar <mehulnahar@Mehuls-MacBook-Pro.local>
2024-10-16 19:22:53 +0530

    common product api

--- src/api/common-product/controllers/common-product.js
+++ src/api/common-product/controllers/common-product.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::common-product.common-product');
+module.exports = createCoreController('api::common-product.common-product', ({strapi}) => ({
+    async findOne(ctx) {
+        
+      const {id} = ctx.params;
+
+
+      const entry = await strapi.db.query('api::common-product.common-product').findOne({
+        
+        where: { id: id },
+        populate: {image: "image"}
+       
+      });
+        const sanitizedResults = await this.sanitizeOutput(entry, ctx);
+    
+        return this.transformResponse(sanitizedResults);
+      }
+}));
