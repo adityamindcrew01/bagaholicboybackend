d3f59b26e6772d6b7b468fe5633983fed0d294e7
Mehul Nahar <mehulnahar@Mehuls-MacBook-Pro.local>
2024-10-21 18:48:40 +0530

    10/21/2024

--- src/api/bags-list/controllers/bags-list.js
+++ src/api/bags-list/controllers/bags-list.js
@@ -6,4 +6,25 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::bags-list.bags-list');
+// module.exports = createCoreController('api::bags-list.bags-list');
+module.exports = createCoreController('api::bags-list.bags-list',({strapi}) => ({
+    async findOne(ctx) {
+        
+      const {id} = ctx.params;
+
+
+      const entry = await strapi.db.query('api::bags-list.bags-list').findOne({
+        
+        where: { id: id },
+        populate: {
+             image: "image" ,
+            image2: "image2"
+    }, 
+       
+       
+      });
+        const sanitizedResults = await this.sanitizeOutput(entry, ctx);
+    
+        return this.transformResponse(sanitizedResults);
+      }
+}))
--- src/api/beauty/controllers/beauty.js
+++ src/api/beauty/controllers/beauty.js
@@ -6,4 +6,26 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::beauty.beauty');
+// module.exports = createCoreController('api::beauty.beauty');
+
+module.exports = createCoreController('api::beauty.beauty',({strapi}) => ({
+    async findOne(ctx) {
+        
+      const {id} = ctx.params;
+
+
+      const entry = await strapi.db.query('api::beauty.beauty').findOne({
+        
+        where: { id: id },
+        populate: {
+             image: "image" ,
+            image2: "image2"
+    }, 
+       
+       
+      });
+        const sanitizedResults = await this.sanitizeOutput(entry, ctx);
+    
+        return this.transformResponse(sanitizedResults);
+      }
+}))
